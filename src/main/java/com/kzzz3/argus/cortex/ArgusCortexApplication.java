e74e049032f085b50877528de479e65421d7863e
Hephaestus <hephaestus@local>
2026-04-03 12:59:38 +0800

    Initial commit
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ src/main/java/com/kzzz3/argus/cortex/ArgusCortexApplication.java
@@ -0,0 +1,13 @@
+package com.kzzz3.argus.cortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusCortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgusCortexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kzzz3/argus/cortex/ArgusCortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzzz3.argus.cortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusCortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
